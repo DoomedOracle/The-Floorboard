d03d16d293fe227b5ce225f53a9bdaba7d8f328e
DoomedOracle <cassroxburgh@gmail.com>
2026-01-21 15:15:02 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  root: './',
+  base: './', // Vital for GitHub Pages subfolders
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
-  server: {
-    port: 3000,
   }
 });
